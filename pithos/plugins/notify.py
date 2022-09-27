66f20a5c6c19ca810ecd8b90eec5a9fb3f4bcfb5
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2022-09-27 19:40:21 -0400

    c

--- pithos/plugins/notify.py
+++ pithos/plugins/notify.py
@@ -84,7 +84,7 @@ class NotifyPlugin(PithosPlugin):
                 icon = self._fallback_icon
 
             notification.set_icon(icon)
-            notification.add_button('_Skip', 'window.next_song')
+            notification.add_button('Skip', 'app.next-song')
             self._app.send_notification(self._app_id, notification)
 
     def on_disable(self):
