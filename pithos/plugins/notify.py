098540a629d059e55c9f230df23eae0408d8cc83
Rick Calixte <10281587+rcalixte@users.noreply.github.com>
2022-09-27 21:38:39 -0400

    c

--- pithos/plugins/notify.py
+++ pithos/plugins/notify.py
@@ -76,6 +76,8 @@ class NotifyPlugin(PithosPlugin):
                     icon_bytes = icon_uri.load_bytes(None)
                     icon = Gio.BytesIcon.new(icon_bytes[0])
                 elif is_flatpak():
+                    icon_path = Gio.File.new_for_uri(song.artUrl)
+                    print(f'{icon_path=}')
                     icon = Gio.Icon.new_for_string(song.artUrl[:6] + song.artUrl[7:])
                     #icon = Gio.ThemedIcon.new(song.artUrl)
                 else:
